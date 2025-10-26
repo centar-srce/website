6c3ea39bd65cc4ff463819f0a2fb70b08cb1acbb
Bojan Čoka <bojan@centarsrce.org>
2025-10-26 22:40:08 +0100

    Consolidate routes

---
+++ src/routes.ts
@@ -0,0 +1,22 @@
+export const ROUTES = {
+	home: '/',
+
+	// Suicide section
+	thinkingAboutSuicide: '/razmisljas-li-o-samoubistvu',
+	problemScale: '/velicina-problema',
+	prejudices: '/predrasude',
+	riskFactors: '/faktori-rizika',
+
+	// How to help section
+	warningSigns: '/kako-pomoci/znaci-upozorenja',
+	whatYouCanDo: '/kako-pomoci/sta-mozete-da-uradite',
+	whoYouCanTurnToForHelp: '/kako-pomoci/kome-se-mozete-obratiti-za-pomoc',
+	prevention: '/kako-pomoci/prevencija',
+	findSupport: '/kako-pomoci/pronadjite-podrsku',
+
+	// TODO: Interpolate in dynamic routes.
+	news: '/vesti',
+
+	volunteering: '/volontiranje',
+	donations: '/donacije',
+} as const;
